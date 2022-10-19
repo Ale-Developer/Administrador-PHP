0d11c100e3682768724ed1b3e3f4fdecff9e0d26
Ale-dev <miguel.espinosa@gmail.com>
2022-10-19 01:08:05 -0300

    Generado el login, listo para la bbdd

--- Vistas/modulos/login.php
+++ Vistas/modulos/login.php
@@ -1,1 +1,38 @@
-<h1>login</h1>
+<body class="bg-dark">
+    <div class="sufee-login d-flex align-content-center flex-wrap">
+        <div class="container">
+            <div class="login-content">
+                <div class="login-logo">
+                    <a href="index.html">
+                        <img class="align-content" src="images/logo.png" alt="">
+                    </a>
+                </div>
+                <div class="login-form">
+                    <form>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" class="form-control" placeholder="Ingrese su email">
+                        </div>
+                        <div class="form-group">
+                            <label>Contraseña</label>
+                            <input type="password" class="form-control" placeholder="Ingrese su contraseña">
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> Recuérdame
+                            </label>
+                            <label class="pull-right">
+                                <a href="#">¿Olvidaste tu contraseña?</a>
+                            </label>
+
+                        </div>
+                        <button type="submit" class="btn btn-success btn-flat m-b-30 m-t-30">Ingresar</button>
+                        <div class="register-link m-t-15 text-center">
+                            <p>¿No tienes cuenta? <a href="#"> Registrate aquí</a></p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
--- Vistas/plantilla.php
+++ Vistas/plantilla.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,30 +26,37 @@
     <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
 
 </head>
+
 <body>
     <?php
-    include "modulos/menu.php";
 
-    echo '<div id="right-panel" class="right-panel">';
+    if (isset($_SESSION["validarSesionBackend"]) && $_SESSION["validarSesionBackend"] === "ok") {
+        include "modulos/menu.php";
 
-    include "modulos/header.php";
-    
-    if (isset($_GET["ruta"])) {
-        if ($_GET["ruta"] == "inicio" || 
-        $_GET["ruta"] == "perfil" ||
-        $_GET["ruta"] == "productos") {
-            include "modulos/" . $_GET["ruta"] . ".php";
+        echo '<div id="right-panel" class="right-panel">';
+
+        include "modulos/header.php";
+
+        if (isset($_GET["ruta"])) {
+            if (
+                $_GET["ruta"] == "inicio" ||
+                $_GET["ruta"] == "perfil" ||
+                $_GET["ruta"] == "productos"
+            ) {
+                include "modulos/" . $_GET["ruta"] . ".php";
+            }
         }
+
+        include "modulos/footer.php";
+
+        echo "</div>";
+    } else {
+        include "modulos/login.php";
     }
-
-    include "modulos/footer.php";
-
-    echo "</div>";
-
     ?>
 
-<!-- Scripts -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
@@ -73,4 +81,5 @@
     <script src="./assets/js/init/fullcalendar-init.js"></script>
 
 </body>
+
 </html>
